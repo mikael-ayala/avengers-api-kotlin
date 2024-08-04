77519ae2dafbb787cfac3f70696122ee23c00098
mikael-ayala <mikael.ayala.santos@gmail.com>
2024-08-04 16:52:45 -0300

    initial commit

---
+++ src/test/kotlin/me/dio/avengers/AvengersApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.dio.avengers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AvengersApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
